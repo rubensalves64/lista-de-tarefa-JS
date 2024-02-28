aa0e413fc965cb4cfef32d5f52fe61247ad43b97
rubensalves64 <rubens.pita2000@gmail.com>
2024-02-28 01:58:56 +0000

    criando a develop

---
+++ script.js
@@ -0,0 +1,27 @@
+/*let pessoa = {
+  nome: "matheus",
+  idade: 26,
+  altura: 1.85,
+  cargo: "programador",
+};
+
+console.log(pessoa);
+let carro = {
+  nome: "bmw 530",
+  cor: "azul",
+  potencia: "220cv",
+};
+console.log(carro);*/
+
+/*let usuarios = [
+  { nome: "matheus", cargo: "programador", status: "ativo" },
+  { nome: "maria", cargo: "programadora", status: "ativo" },
+  { nome: "jose", cargo: "RH", status: "ativo" },
+];
+console.log(usuarios);*/
+
+//template strings
+let nome = "matheus";
+
+let mensagem = `Meu nome e ${nome}`;
+console.log(mensagem);
